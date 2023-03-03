d1849a762cb6b92faf4ccc053235585d31397c05
Hemant D <hemant132us@gmail.com>
2023-03-03 12:38:59 +0530

    new firebase update

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,36 @@
-`const bot = '../assets/bot.svg'
+const bot = '../assets/bot.svg'
 const user = '../assets/user.svg'
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
-const msgs =[]
-var firebaseConfig = {
- apiKey: "AIzaSyArIyFKvHKgy5ZJWFeuDJyLpqd1GApsrrY",
- authDomain: "chatbot-ca9b4.firebaseapp.com",
- databaseURL: "https://chatbot-ca9b4-default-rtdb.firebaseio.com",
- projectId: "chatbot-ca9b4",
- storageBucket: "chatbot-ca9b4.appspot.com",
- messagingSenderId: "329318236351",
- appId: "1:329318236351:web:4ab6ab3ce70a6899f8860a"
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyCAzupEfPnjlZa8k4p9lXMqNhFwe_JSc3Y",
+    authDomain: "openai-41fb8.firebaseapp.com",
+    databaseURL: "https://openai-41fb8-default-rtdb.firebaseio.com",
+    projectId: "openai-41fb8",
+    storageBucket: "openai-41fb8.appspot.com",
+    messagingSenderId: "807371754493",
+    appId: "1:807371754493:web:99e627918257a663026794"
+  };
 firebase.initializeApp(firebaseConfig);
 
  var database = firebase.database();
  var prompt_val = '';
- var ikey='-NNslc0QdjjEYGQlGaAG';
+ var msgs = '';
+
+ var ikey='-NOBin7n3HsYRmL342z9';
  console.log(ikey)
- database.ref().child(\`/botform/\${ikey}/\`).once('value', function(snapshot) {
+ database.ref().child('/botform/-NOBin7n3HsYRmL342z9').once('value', function(snapshot) {
      
          var val = snapshot.val();
-         prompt_val = val.prompt 
-        //ikey = data.key
-      
+         msgs = val.prompt 
+        //ikey = data.key   
  });
-
+console.log(msgs)
+var msgs1 =''
+const msgdata = []
 let loadInterval
-
 function loader(element) {
     element.textContent = ''
-
     loadInterval = setInterval(() => {
         // Update the text content of the loading indicator
         element.textContent += '.';
@@ -57,43 +57,46 @@ function typeText(element, text) {
 // generate unique ID for each message div of bot
 // necessary for typing text effect for that specific reply
 // without unique ID, typing text will work on every element
+
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
     const hexadecimalString = randomNumber.toString(16);
-    var result = \`id-\${timestamp}-\${hexadecimalString}\`;
+    var result = `id-${timestamp}-${hexadecimalString}`;
     return  result;
-
-
-   // console.log(timestamp)
+    // console.log(timestamp)
     //console.log("resulttimestamp)
 }
 
 function chatStripe(isAi, value, uniqueId) {
     return (
-       \`
-        <div class="wrapper \${isAi && 'ai'}">
+       `
+        <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
                 <div class="profile">
                     <img 
-                      src=\${isAi ? bot : user} 
-                      alt="\${isAi ? 'bot' : 'user'}" 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
                     />
                 </div>
-                <div class="message" id=\${uniqueId}>\${value}</div>
+                <div class="message" id=${uniqueId}>${value}</div>
             </div>
         </div>
-    \`
+        
+        `
     )
 }
+
+
 function getInputVal(id) {
     return document.getElementById(id).value;
 }
 
     
+//var results = msgs.concat(msgs1)
+//msgdata.push(results)
 
 const handleSubmit = async (e) => {
-    console.log(prompt_val)
 
    // const finalkey = ikey
 
@@ -101,7 +104,8 @@ const handleSubmit = async (e) => {
   // .ref().child('/botform/' + finalkey);
    
    var message = getInputVal('promptmsgs');
-  
+  msgs = msgs.concat(getInputVal('promptmsgs'))
+
 
 
   // messagesRef.update({prompt:message});
@@ -110,8 +114,6 @@ const handleSubmit = async (e) => {
     e.preventDefault()
 
     const data = new FormData(form)
-    msgs.push(prompt_val + getInputVal('promptmsgs'))
-
     // user's chatstripe
     chatContainer.innerHTML += chatStripe(false,data.get('prompt'))
 
@@ -133,37 +135,43 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:3000/', {
+    const response = await fetch('https://chat-server-o6nz.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            prompt:\`\${msgs}\`
+            prompt: msgs
         })
     })
-    console.log(prompt_val)
+   // console.log(prompt_val)
     clearInterval(loadInterval)
     messageDiv.innerHTML = " "
-
+    
     if (response.ok) {
         const data = await response.json();
         const parsedData = data.bot.trim()
         typeText(messageDiv, parsedData)
        // confirm
+      msgs1 = parsedData
+     
 
-        console.log(msgs)
-    } else {
+
+  } else {
         const err = await response.text()
 
         messageDiv.innerHTML = "Something went wrong"
         alert(err)
     }
+    msgs = msgs.concat(msgs1)
+    console.log(msgs)
+   
 }
+
 
 form.addEventListener('submit', handleSubmit)
 form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
-        handleSubmit(e)
+     handleSubmit(e)
     }
-})`
+})
